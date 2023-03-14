e1ad1fc2412aa8d38b7238103d6d5c7a2c2c5916
Ximena Alcaman <flawergerl.x@gmail.com>
2023-03-14 03:30:31 -0300

    initial commit

---
+++ programas_ramificados.py
@@ -0,0 +1,10 @@
+num_1 = int(input('Type an integer: '))
+num_2 = int(input('Type another integer: '))
+
+if num_1 > num_2:
+    print('The first number is greater than the second')
+elif num_1 < num_2:
+    print('The second number is greater than the first')
+else:
+    print('The numbers are equal')
+
